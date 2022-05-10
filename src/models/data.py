3489ba051b124726133bb8fec1f48528ff33be8d
cwreed <connor.william.reed@gmail.com>
2022-05-09 21:25:18 -0400

    update flood data class weights and change how permanent water samples are used for training

--- src/models/data.py
+++ src/models/data.py
@@ -1,6 +1,7 @@
 from pathlib import Path
 from typing import Optional
 import pickle
+import random
 
 import numpy as np
 import torch
@@ -81,16 +82,27 @@ class FloodClassificationDataset(Dataset):
         self,
         data_folder: Path,
         subset: str,
-        use_perm_water: bool
+        perm_water_proportion: Optional[float]
     ):
         self.data_folder = data_folder
         self.features_dir = data_folder / 'features' / C2SDFOEngineer.dataset
 
-        assert subset in ['train', 'validation', 'test']
+        if subset not in ['train', 'validation', 'test']:
+            raise ValueError(f"subset should be one of ['train', 'validation', 'test']--got {subset}")
+        
         self.subset = subset
-        self.use_perm_water = use_perm_water
 
-        self.data_files, self.lookup, self.normalizing_dict = self.load_files(self.features_dir, self.subset, self.use_perm_water)
+        if (perm_water_proportion > 1.0) | (perm_water_proportion < 0.0):
+            raise ValueError(f"perm_water_proportion should be in the range [0.0, 1.0]--got {perm_water_proportion}")
+
+        self.perm_water_proportion = perm_water_proportion
+
+        """Set these here to ensure permanent water sampling is reproducible"""
+        random.seed(2022)
+        np.random.seed(2022)
+        torch.manual_seed(2022)
+
+        self.data_files, self.lookup, self.normalizing_dict = self.load_files(self.features_dir, self.subset, self.perm_water_proportion)
 
     def __getitem__(self, index: int) -> tuple[torch.Tensor, int, int]:
         file, file_index = self.lookup[index]
@@ -111,7 +123,12 @@ class FloodClassificationDataset(Dataset):
         return len(self.lookup)
 
     @staticmethod
-    def load_files(features_dir: Path, subset: str, use_perm_water: bool) -> tuple[list[Path], list[tuple[str, int]], Optional[dict]]:
+    def load_files(
+        features_dir: Path, 
+        subset: str,
+        perm_water_proportion: Optional[float]
+    ) -> tuple[list[Path], list[tuple[str, int]], Optional[dict]]:
+
         data_files = list((features_dir / subset).glob('*.pkl'))
         lookup_file = features_dir / subset / 'lookup.ref'
         normalizing_dict_file = features_dir / 'normalizing_dict.pkl'
@@ -126,17 +143,25 @@ class FloodClassificationDataset(Dataset):
         else:
             normalizing_dict = None
 
-        if not use_perm_water:
+        if perm_water_proportion < 1.:
+            """Selectively add permanent water samples to the lookup"""
             non_perm_water_indices = []
+            select_perm_water_indices = []
+
             for i, (file, file_index) in enumerate(lookup):
                 filepath = features_dir / subset / f'{file}.pkl'
                 with filepath.open('rb') as f:
                     data_instance = pickle.load(f)[file_index]
-                    if data_instance.perm_water == 0:
+                    if data_instance.perm_water == 1:
+                        if random.random() < perm_water_proportion:
+                            select_perm_water_indices.append(i)
+                    else:
                         non_perm_water_indices.append(i)
 
-            lookup = [lookup[i] for i in non_perm_water_indices]
-        
+            non_perm_water_lookup = [lookup[i] for i in non_perm_water_indices]
+            perm_water_lookup = [lookup[i] for i in select_perm_water_indices]
+            lookup = [*non_perm_water_lookup, *perm_water_lookup]
+ 
         return data_files, lookup, normalizing_dict
 
     def normalize(self, array: np.ndarray) -> np.ndarray:
@@ -164,12 +189,19 @@ class FloodClassificationDataset(Dataset):
         return output_tuple[0].shape[0]
     
     @property
-    def output_class_samples(self) -> tuple[int, int]:
+    def output_class_samples(self) -> np.ndarray:
         """Calculates the number of samples belonging to each flood_label class"""
         assert len(self.data_files) > 0, "No files loaded"
-        class_counts = [0] * (self.num_output_classes + 1)
+
+        """Count classes for both present and past flood labels"""
+        class_counts = np.zeros((self.num_output_classes + 1, self.num_output_classes + 1))
         for output_tuple in self:
-            class_counts[int(output_tuple[1])] += 1
+            """
+            Columns: Flood present, flood past
+            Rows: Negative label (0), positive label (1)
+            """
+            class_counts[int(output_tuple[1]), int(output_tuple[2])] += 1
+            
         return class_counts
 
     @property
@@ -178,5 +210,5 @@ class FloodClassificationDataset(Dataset):
         assert len(self.data_files) > 0, "No files loaded"
         N = self.__len__()
         class_numbers = self.output_class_samples
-        weights = [(N / class_numbers[int(i[1])]) for i in self]
+        weights = [(N / class_numbers[int(i[1]), int(i[2])]) for i in self]
         return weights
