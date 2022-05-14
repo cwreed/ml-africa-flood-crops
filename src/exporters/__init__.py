36fe255afca80b21c9551d7ac3b5aa272575ee8e
cwreed <connor.william.reed@gmail.com>
2022-05-13 23:49:22 -0400

    add regional exporters to module

--- src/exporters/__init__.py
+++ src/exporters/__init__.py
@@ -1,7 +1,9 @@
 from .cropharvest import CropHarvestExporter
 from .cloud_to_street_dfo import C2SDFOExporter
 from .sentinel_2.cropharvest import CropHarvestSentinel2Exporter
+from .sentinel_2.region import RegionalSentinel2Exporter
 from .sentinel_1.cloud_to_street_dfo import C2SDFOSentinel1Exporter
+from .sentinel_1.region import RegionalSentinel1Exporter
 
 from ..utils.regions import STR2BB, REGIONS
 
@@ -10,6 +12,8 @@ __all__ = [
     "C2SDFOExporter",
     "CropHarvestSentinel2Exporter",
     "C2SDFOSentinel1Exporter",
+    "RegionalSentinel2Exporter",
+    "RegionalSentinel1Exporter",
     "STR2BB",
     "REGIONS"
 ]
